d55b467a882eafadf559a5e9bf977660d3bc130f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-25 23:15:14 +0530

    Grade distribution

--- mini_project.py
+++ mini_project.py
@@ -2,3 +2,6 @@ import matplotlib.pyplot as plt
 students = ["Aman","Riya","Kabir","Neha","Raj"]
 marks = [78,92,67,88,75]
 attendence = [85,95,70,90,80]
+#grade distribution
+grades = ["A","B","C"]
+grade_count = [2,2,1]
