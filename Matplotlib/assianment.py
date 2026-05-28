457cf82dc3609015d0b730503e5d4531f1839711
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-28 22:48:07 +0530

    plot axvline

--- Matplotlib/assianment.py
+++ Matplotlib/assianment.py
@@ -4,3 +4,4 @@ import seaborn as sns
 scores = np.random.normal(loc = 70,scale = 10,size = 200)
 mean_score = np.mean(scores)
 plt.hist(scores,bins = 15)
+plt.axvline(mean_score,linestyle = "--",color = "red",label = f" mean = {mean_score:.2f}")
